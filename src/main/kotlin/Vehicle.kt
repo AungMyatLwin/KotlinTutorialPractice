a736d6048151ec1dea52ac3eed2561a8bdff70f8
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-04-13 08:07:46 +0630

    Extends interface Engine

--- src/main/kotlin/Vehicle.kt
+++ src/main/kotlin/Vehicle.kt
@@ -1,3 +1,11 @@
+
+// interface(to share behaviour) but not actual code
+
+interface Engine{
+    fun startEnginge()
+}
+
+
 // parent class
 open class Vehicle(val name: String, val color:String): Vehicles() {
    open fun move(){
@@ -14,10 +22,14 @@ open class Vehicle(val name: String, val color:String): Vehicles() {
     override fun stop2() {
         println("Implemented from ABS cls")
     }
+
+    override fun startEnginge() {
+        TODO("Not yet implemented")
+    }
 }
 
 // abstract class
-abstract class Vehicles{
+abstract class Vehicles:Engine{
     abstract fun move2()
     abstract fun stop2()
 }
